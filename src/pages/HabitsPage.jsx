7e85587f68c57e9ffa6b5816185ca429606d4cf1
wildfont <jordifont4@gmail.com>
2026-05-05 00:32:22 +0200

    i forget to commit before....

--- src/pages/HabitsPage.jsx
+++ src/pages/HabitsPage.jsx
@@ -1,6 +1,43 @@
+import { useState, useEffect } from "react"
+import api from "../services/habits-service"
+import HabitForm from "./HabitForm"
+
+
+
+
 function HabitsPage() {
+
+  const [habits, setHabits] = useState([])
+
+  function fetchHabits() {
+    api.get("/habits")
+  .then((response) => setHabits(response.data))    
+  }
+
+  function handleDelete(id){
+    api.delete("/habits/"+id)
+    .then(()=>{
+      fetchHabits()
+    })
+  }
+
+  useEffect(()=>{ 
+  fetchHabits()
+  },[])
+  
   return (
-    <div>HabitsPage</div>
+    <div>
+      {habits.map((habit) => (
+       <div key={habit.id}>
+        <p >{habit.name}</p>
+        <button onClick={() => handleDelete(habit.id)}>Delete</button>
+        </div>
+    ))} 
+    <HabitForm fetchHabits={fetchHabits}/>
+     </div>
   )
+   
+
+    
 }
 export default HabitsPage
